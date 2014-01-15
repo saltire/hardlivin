fe6b1710b817c15332b5222eb2c291fa62a01626
saltire sable <saltiresable@gmail.com>
2014-01-14 20:35:59 -0500

    Add title animation easter egg

---
+++ hardlivin/static/title.js
@@ -0,0 +1,14 @@
+function colourRandomLetter() {
+	var $nohover = $('.title div').not('.hover');
+	var random = Math.floor(Math.random() * $nohover.length);
+	$('.hover').removeClass('hover')
+	$nohover.eq(random).addClass('hover');
+	setTimeout(colourRandomLetter, 300);
+}
+
+$(function() {
+	$('.title div').click(function() {
+		$('.title div').off();
+		colourRandomLetter();
+	});
+});
