ac95a69f7775eb12a0a622faae362625513210ca
saltire sable <saltiresable@gmail.com>
2014-01-29 01:43:49 -0500

    Refactored request_square, added request_line, started continuous scroll

--- led/4bit.py
+++ led/4bit.py
@@ -19,26 +19,43 @@ class ImageServer:
             if code == 'A':
                 print 'got send_image code'
                 self.send_image(random.choice(self.images))
+            elif code == 'B':
+                print 'got send_col code'
+                self.send_cols(random.choice(self.images))
+
+
+    def convert_colour(self, r, g, b, a):
+        return (r >> 4, g >> 4, b >> 4) if a else (0, 0, 0)
 
 
     def send_image(self, imgpath):
         img = Image.open(imgpath)
         print 'sending {0}...'.format(os.path.basename(imgpath))
 
-        odd = True
-        for r, g, b, a in img.getdata():
-            colour = (r >> 4, g >> 4, b >> 4) if a else (0, 0, 0)
+        # this next line breaks the list of pixels into groups of 2
+        for colour1, colour2 in zip(*[iter(img.getdata())] * 2):
+            r1, g1, b1 = self.convert_colour(*colour1)
+            r2, g2, b2 = self.convert_colour(*colour2)
 
-            if odd:
-                r1, g1, b1 = colour
-            else:
-                r2, g2, b2 = colour
-                self.arduino.write_ints(r1 << 4 | g1, b1 << 4 | r2, g2 << 4 | b2)
-                self.arduino.serial.read(1)
-
-            odd = not odd
+            self.arduino.write_ints(r1 << 4 | g1, b1 << 4 | r2, g2 << 4 | b2)
+            self.arduino.serial.read(1)
 
         print 'done.'
+
+
+    def send_cols(self, imgpath):
+        img = Image.open(imgpath)
+        pix = img.load()
+        w, h = img.size
+        for x in xrange(w):
+            print 'sending column', x
+            if x > 0:
+                self.arduino.serial.read(1)
+            for y in xrange(0, h, 2):
+                r1, g1, b1 = self.convert_colour(*pix[x, y])
+                r2, g2, b2 = self.convert_colour(*pix[x, y + 1])
+                self.arduino.write_ints(r1 << 4 | g1, b1 << 4 | r2, g2 << 4 | b2)
+
 
 
 if __name__ == '__main__':
--- led/led4bit.ino
+++ led/led4bit.ino
@@ -14,33 +14,43 @@ void setup() {
 	Serial.begin(9600);
 }
 
-void request_square(int16_t image[12][12]) {
-	Serial.write('A');
+void request_column(int16_t col[]) {
+	Serial.write('B');
 
-	uint8_t x = 0, y = 0;
-
+	uint8_t y = 0;
 	while (y < 12) {
 		if (Serial.available() >= 3) {
 			uint8_t byte1 = Serial.read();
 			uint8_t byte2 = Serial.read();
 			uint8_t byte3 = Serial.read();
 
-			image[x][y] = matrix.Color888(
+			col[y] = matrix.Color888(
 					byte1 / 16 << 4,
 					byte1 % 16 << 4,
 					byte2 / 16 << 4, 1);
-			image[x + 1][y] = matrix.Color888(
+			col[y + 1] = matrix.Color888(
 					byte2 % 16 << 4,
 					byte3 / 16 << 4,
 					byte3 % 16 << 4, 1);
+			y += 2;
+		}
+	}
+}
 
-			Serial.write(1);
+void request_square(int16_t image[12][12]) {
+	int16_t col[12];
+	for (uint8_t x = 0; x < 12; x++) {
+		request_column(col);
+		for (uint8_t y = 0; y < 12; y++) {
+			image[x][y] = col[y];
+		}
+	}
+}
 
-			x += 2;
-			if (x == 12) {
-				x = 0;
-				y += 1;
-			}
+void draw_square(int8_t xoff, int8_t yoff, int16_t image[12][12]) {
+	for (uint8_t y = 0; y < 12; y++) {
+		for (uint8_t x = 0; x < 12; x++) {
+			matrix.drawPixel(x + xoff, y + yoff, image[x][y]);
 		}
 	}
 }
@@ -49,20 +59,45 @@ void scroll_left(uint8_t repeat, uint8_t speed) {
 	int16_t image[12][12];
 	request_square(image);
 
-	int8_t yoff = 2;
 	for (uint8_t i = 0; i < repeat; i++) {
 		for (int8_t xoff = 32; xoff >= -12; xoff--) {
 			matrix.fillScreen(0);
-			for (uint8_t y = 0; y < 12; y++) {
-				for (uint8_t x = 0; x < 12; x++) {
-					matrix.drawPixel(x + xoff, y + yoff, image[x][y]);
-				}
-			}
+			draw_square(xoff, 2, image);
 			delay(speed);
 		}
 	}
 }
 
+void scroll_multiple(uint8_t repeat, uint8_t speed) {
+	//int16_t image1[12][12];
+	int16_t image2[12][12];
+	int16_t image3[12][12];
+	int16_t image4[12][12] = {0};
+	//request_square(image1);
+	request_square(image2);
+	request_square(image3);
+
+	int16_t col[12];
+
+	for (int8_t xoff = 2; xoff >= -24; xoff--) {
+		matrix.fillScreen(0);
+		//draw_square(xoff, 2, image1);
+		draw_square(xoff + 14, 2, image2);
+		draw_square(xoff + 28, 2, image3);
+
+		if (xoff < -10) {
+			request_column(col);
+			for (uint8_t y = 0; y < 12; y++) {
+				image4[xoff + 10][y] = col[y];
+			}
+			draw_square(xoff + 42, 2, image4);
+		}
+
+		delay(speed);
+	}
+}
+
 void loop() {
 	scroll_left(1, 100);
+	//scroll_multiple(1, 100);
 }
