298606b8bdaa1a8b257a8073fd33cf9242858236
saltire sable <saltiresable@gmail.com>
2014-01-29 00:20:50 -0500

    LED Arduino code now uses functions; scrolls random images

--- led/4bit.py
+++ led/4bit.py
@@ -1,33 +1,46 @@
 import os
+import random
 
 from PIL import Image
 
 from arduino import Arduino
 
 
-ard = Arduino('COM7', 9600)
+class ImageServer:
+    def __init__(self, port='COM7'):
+        imgdir = 'images'
+        self.images = [os.path.join(imgdir, fname) for fname in os.listdir(imgdir)]
+        self.arduino = Arduino(port, 9600)
 
-for fname in os.listdir('images'):
-    img = Image.open('images/{0}'.format(fname))
-    print 'sending {0}...'.format(fname)
 
-    odd = True
-    for r, g, b, a in img.getdata():
-        colour = (r >> 4, g >> 4, b >> 4) if a else (0, 0, 0)
+    def run(self):
+        while True:
+            code = self.arduino.serial.read(1)
+            if code == 'A':
+                print 'got send_image code'
+                self.send_image(random.choice(self.images))
 
-        if odd:
-            r1, g1, b1 = colour
-        else:
-            r2, g2, b2 = colour
-            ard.write_ints(r1 << 4 | g1,
-                           b1 << 4 | r2,
-                           g2 << 4 | b2)
 
-            ard.serial.read(1)
+    def send_image(self, imgpath):
+        img = Image.open(imgpath)
+        print 'sending {0}...'.format(os.path.basename(imgpath))
 
-        odd = not odd
+        odd = True
+        for r, g, b, a in img.getdata():
+            colour = (r >> 4, g >> 4, b >> 4) if a else (0, 0, 0)
 
-    print 'done.'
-    break
+            if odd:
+                r1, g1, b1 = colour
+            else:
+                r2, g2, b2 = colour
+                self.arduino.write_ints(r1 << 4 | g1, b1 << 4 | r2, g2 << 4 | b2)
+                self.arduino.serial.read(1)
 
-raw_input()
+            odd = not odd
+
+        print 'done.'
+
+
+if __name__ == '__main__':
+    server = ImageServer()
+    server.run()
--- led/led4bit.ino
+++ led/led4bit.ino
@@ -9,22 +9,22 @@
 #define C   A2
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
-uint8_t x = 0, y = 0;
-int8_t xoff = 10, yoff = 2;
-int16_t image[12][12];
-
 void setup() {
 	matrix.begin();
 	Serial.begin(9600);
 }
 
-void loop() {
-	if (x < 12 || y < 12) {
-		while (Serial.available() >= 3 && (x < 12 || y < 12)) {
+void request_square(int16_t image[12][12]) {
+	Serial.write('A');
+
+	uint8_t x = 0, y = 0;
+
+	while (y < 12) {
+		if (Serial.available() >= 3) {
 			uint8_t byte1 = Serial.read();
 			uint8_t byte2 = Serial.read();
 			uint8_t byte3 = Serial.read();
-			
+
 			image[x][y] = matrix.Color888(
 					byte1 / 16 << 4,
 					byte1 % 16 << 4,
@@ -33,27 +33,36 @@ void loop() {
 					byte2 % 16 << 4,
 					byte3 / 16 << 4,
 					byte3 % 16 << 4, 1);
-			
+
 			Serial.write(1);
-			
+
 			x += 2;
 			if (x == 12) {
+				x = 0;
 				y += 1;
-				if (y < 12) {
-					x = 0;
-				}
 			}
 		}
-	
-	} else {
-		for (xoff = 32; xoff > -12; xoff--) {
+	}
+}
+
+void scroll_left(uint8_t repeat, uint8_t speed) {
+	int16_t image[12][12];
+	request_square(image);
+
+	int8_t yoff = 2;
+	for (uint8_t i = 0; i < repeat; i++) {
+		for (int8_t xoff = 32; xoff >= -12; xoff--) {
 			matrix.fillScreen(0);
-			for (y = 0; y < 12; y++) {
-				for (x = 0; x < 12; x++) {
+			for (uint8_t y = 0; y < 12; y++) {
+				for (uint8_t x = 0; x < 12; x++) {
 					matrix.drawPixel(x + xoff, y + yoff, image[x][y]);
 				}
 			}
-			delay(100);
+			delay(speed);
 		}
 	}
+}
+
+void loop() {
+	scroll_left(1, 100);
 }
